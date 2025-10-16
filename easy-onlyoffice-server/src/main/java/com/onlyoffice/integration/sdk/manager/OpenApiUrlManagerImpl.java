6e94a03592eac12f7d68e873cc83f5caafa9a354
keguang <1249484629@qq.com>
2025-10-16 21:26:18 +0800

    优化OpenApiUrlManagerImpl类，属性都更换成openApi链路

--- easy-onlyoffice-server/src/main/java/com/onlyoffice/integration/sdk/manager/OpenApiUrlManagerImpl.java
+++ easy-onlyoffice-server/src/main/java/com/onlyoffice/integration/sdk/manager/OpenApiUrlManagerImpl.java
@@ -1,11 +1,26 @@
 package com.onlyoffice.integration.sdk.manager;
 
+import com.onlyoffice.integration.documentserver.storage.FileStoragePathBuilder;
+import com.onlyoffice.manager.document.DocumentManager;
 import com.onlyoffice.manager.settings.SettingsManager;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
+
 @Component
-public class OpenApiUrlManagerImpl extends UrlManagerImpl {
-    
+public class OpenApiUrlManagerImpl extends UrlManagerImpl implements InitializingBean {
+
+    @Autowired
+    @Qualifier("openApiFileStorage")
+    private FileStoragePathBuilder storagePathBuilderOverride;
+
+    @Autowired
+    @Qualifier("openApiDocumentManagerImpl")
+    private DocumentManager documentManagerOverride;
+
     public OpenApiUrlManagerImpl(SettingsManager settingsManager) {
         super(settingsManager);
     }
@@ -14,5 +29,16 @@ public class OpenApiUrlManagerImpl extends UrlManagerImpl {
     public String getCreateUrl(String internalFileName) {
         return null;
     }
-    
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Field storagePathBuilderField = UrlManagerImpl.class.getDeclaredField("storagePathBuilder");
+        storagePathBuilderField.setAccessible(true);
+        storagePathBuilderField.set(this, storagePathBuilderOverride);
+
+        Field documentManagerField = UrlManagerImpl.class.getDeclaredField("documentManager");
+        documentManagerField.setAccessible(true);
+        documentManagerField.set(this, documentManagerOverride);
+    }
+
 }
