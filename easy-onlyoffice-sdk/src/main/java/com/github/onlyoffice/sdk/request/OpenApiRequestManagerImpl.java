e30604fe09d878f596267e0d0524332da2a99410
keguang <1249484629@qq.com>
2025-11-04 21:26:22 +0800

    版本升级到1.0.5，改造点： 1.OpenApiRequestManager请求源文件在线编辑链接地址的请求参数增加可以传请求头数据

--- easy-onlyoffice-sdk/src/main/java/com/github/onlyoffice/sdk/request/OpenApiRequestManagerImpl.java
+++ easy-onlyoffice-sdk/src/main/java/com/github/onlyoffice/sdk/request/OpenApiRequestManagerImpl.java
@@ -8,9 +8,15 @@ import com.onlyoffice.manager.request.DefaultRequestManager;
 import com.onlyoffice.manager.security.JwtManager;
 import com.onlyoffice.manager.settings.SettingsManager;
 import com.onlyoffice.manager.url.UrlManager;
+import com.onlyoffice.model.common.RequestEntity;
+import com.onlyoffice.model.settings.HttpClientSettings;
 import com.onlyoffice.model.settings.security.Security;
 import lombok.SneakyThrows;
 import org.apache.commons.io.IOUtils;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.HttpEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -18,7 +24,10 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriBuilder;
 
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.github.onlyoffice.sdk.constants.OpenApiUrl.Path.BASE;
 import static com.github.onlyoffice.sdk.constants.OpenApiUrl.Path.EDITOR_LINK;
@@ -27,58 +36,86 @@ import static com.github.onlyoffice.sdk.constants.OpenApiUrl.QUERY_PARAM.SRC;
 import static com.github.onlyoffice.sdk.constants.PropertyKey.EASY_ONLYOFFICE_SERVER_URL;
 import static org.springframework.web.util.DefaultUriBuilderFactory.EncodingMode.URI_COMPONENT;
 
+/**
+ * easy-onlyoffice扩展接口实现类
+ * 
+ * @author keguang
+ */
 public class OpenApiRequestManagerImpl extends DefaultRequestManager implements OpenApiRequestManager {
-    
+
+    private Method executeRequest;
+
+    private Method createPostRequest;
+
     public OpenApiRequestManagerImpl(UrlManager urlManager, JwtManager jwtManager, SettingsManager settingsManager) {
         super(urlManager, jwtManager, settingsManager);
-    }
-    
-    @SneakyThrows
-    @Override
-    public String editorLink(String srcUrl, String callbackUrl) {
-        
-        DefaultUriBuilderFactory builderFactory = new DefaultUriBuilderFactory();
-        builderFactory.setEncodingMode(URI_COMPONENT);
-        
-        String url = getSettingsManager().getSetting(EASY_ONLYOFFICE_SERVER_URL) + BASE + EDITOR_LINK;
-        UriBuilder builder = builderFactory.uriString(url);
-        
-        MultiValueMap<String, String> queryParam = new LinkedMultiValueMap<>();
-        queryParam.add(SRC, srcUrl);
-        queryParam.add(CALLBACK, callbackUrl);
-        
-        builder.queryParams(queryParam);
-        url = builder.build().toString();
-        
-        return super.executeGetRequest(url, new DefaultCallback<>(new TypeReference<String>() {}));
+        initSuperMethodReference();
     }
 
     @SneakyThrows
     @Override
-    public String editorLink(LinkRequest linkRequest) {
+    public String editorLink(String srcUrl, String callbackUrl, Header... headers) {
+        DefaultUriBuilderFactory builderFactory = new DefaultUriBuilderFactory();
+        builderFactory.setEncodingMode(URI_COMPONENT);
+
+        String url = getSettingsManager().getSetting(EASY_ONLYOFFICE_SERVER_URL) + BASE + EDITOR_LINK;
+        UriBuilder builder = builderFactory.uriString(url);
+
+        MultiValueMap<String, String> queryParam = new LinkedMultiValueMap<>();
+        queryParam.add(SRC, srcUrl);
+        queryParam.add(CALLBACK, callbackUrl);
+
+        builder.queryParams(queryParam);
+        url = builder.build().toString();
+
+        HttpGet httpGet = new HttpGet(url);
+        addHeaders(httpGet, headers);
+
+        return (String) executeRequest.invoke(this, httpGet, null, new DefaultCallback<>(new TypeReference<String>() {}));
+    }
+
+    @SneakyThrows
+    @Override
+    public String editorLink(LinkRequest linkRequest, Header... headers) {
         Security security = Security.builder()
                 .key(getSettingsManager().getSecurityKey())
                 .header(getSettingsManager().getSecurityHeader())
                 .prefix(getSettingsManager().getSecurityPrefix())
                 .build();
-        
+
         String url = getSettingsManager().getSetting(EASY_ONLYOFFICE_SERVER_URL) + BASE + EDITOR_LINK;
-        
-        return super.executePostRequest(url, linkRequest, security, null,
-                new DefaultCallback<>(new TypeReference<String>() {}));
+
+        HttpPost httpPost = (HttpPost) createPostRequest.invoke(this, url, linkRequest, security);
+        addHeaders(httpPost, headers);
+
+        return (String) executeRequest.invoke(this, httpPost, null, new DefaultCallback<>(new TypeReference<String>() {}));
     }
-    
-    
+
+    private void addHeaders(HttpUriRequest request, Header... headers) {
+        Optional.ofNullable(headers).map(Stream::of).ifPresent(stream -> stream.forEach(request::addHeader));
+    }
+
+    @SneakyThrows
+    private void initSuperMethodReference() {
+        executeRequest = DefaultRequestManager.class.getDeclaredMethod("executeRequest", HttpUriRequest.class,
+                HttpClientSettings.class, Callback.class);
+        executeRequest.setAccessible(true);
+        createPostRequest = DefaultRequestManager.class.getDeclaredMethod("createPostRequest", String.class,
+                RequestEntity.class, Security.class);
+        createPostRequest.setAccessible(true);
+    }
+
+
     public static class DefaultCallback<Result> implements Callback<Result> {
 
         public static ObjectMapper objectMapper = new ObjectMapper();
-        
+
         private TypeReference<Result> typeReference;
-        
+
         public DefaultCallback(TypeReference<Result> typeReference) {
             this.typeReference = typeReference;
         }
-        
+
         @Override
         public Result doWork(Object response) throws Exception {
             InputStream in = ((HttpEntity) response).getContent();
@@ -92,7 +129,7 @@ public class OpenApiRequestManagerImpl extends DefaultRequestManager implements 
             }
             return objectMapper.readValue(content, typeReference);
         }
-        
+
     }
-    
+
 }
